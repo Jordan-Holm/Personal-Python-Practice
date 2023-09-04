720a3c2b08930a12deed5ee473a85591b797b314
Jordan Holm <Jordanholm12@gmail.com>
2023-09-04 15:28:00 -0600

    Create character

---
+++ Read text file project/DnD_character_menu.py
@@ -0,0 +1,3 @@
+from DnD_character_sorting import sort_characters
+
+print(sort_characters())
---
+++ Read text file project/DnD_create_character.py
@@ -0,0 +1,22 @@
+character_file = open("D&D Characters.txt", 'a')
+from DnD_character_sorting import sort_characters
+
+def create_character():
+    character_dict = {'Name': '', 'Player': '', 'Class': '', 'Sub-Class': '', 'Race': '', 'Level': ''}
+    
+    for key in character_dict:
+        value = input(f"Please enter {key}: ")
+        character_dict[key] = value
+
+    return character_dict
+
+def add_character_to_character_list():
+    new_character = create_character()
+    character_file.write("\n")
+    for key, value in new_character.items():
+        if key == "Level":
+            character_file.write(f"{key}: {value}")
+        else:
+            character_file.write(f"{key}: {value}\n")
+
+    character_file.close()
