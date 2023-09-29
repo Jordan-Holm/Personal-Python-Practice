107e244111bc43f4e59fb18df4f745e52773dffd
Jordan Holm <Jordanholm12@gmail.com>
2023-09-29 07:42:13 -0600

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,4 +1,17 @@
-for i in range(5):
-    print("&&")
-    for j in range(3):
-        print("*")
+class_grades = [[0,53,84], [33,87,79], [75,100,94]]
+
+for student_grades in class_grades:
+    test_number = 1
+    for test_grade in student_grades:
+        test_result = f"%{test_grade}" if test_grade >= 60 else "FAILED"
+        if test_grade >= 90:
+            test_letter_grade = "A"
+        elif test_grade >= 80:
+            test_letter_grade = "B"
+        elif test_grade >= 70:
+            test_letter_grade = "C"
+        else:
+            test_letter_grade = "F"
+        print(f"Test #{test_number} Grade: {test_letter_grade} {test_result}", end=", ")
+        test_number += 1
+    print()
